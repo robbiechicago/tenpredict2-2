f6571c5feda8b708bcf4bb9b76163b0764e23164
Rob Holloway <robholloway@robs-mbp.home>
2018-10-14 21:58:08 +0100

    removed best rank box as it aint workin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,30 +12,30 @@
             </div>
 
             <div class="row ">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="home-data-box">
                         <div class="home-data-box-heading">Total Points</div>
                         <div class="home-data-box-data">{{ $my_tot_points }}</div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="home-data-box">
                         <div class="home-data-box-heading">League Position</div>
                         <div class="home-data-box-data">{{ $my_league_pos }}</div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="home-data-box">
                         <div class="home-data-box-heading">Highest Score</div>
                         <div class="home-data-box-data">{{ $high_score }}</div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                {{-- <div class="col-md-3">
                     <div class="home-data-box">
                         <div class="home-data-box-heading">Best Weekly Place</div>
                         <div class="home-data-box-data">{{ $best_rank }} (week{{ $best_week_s }} {{ $best_weeks_string }})</div>
                     </div>
-                </div>
+                </div> --}}
             </div>
 
             <table id="home-weeks-table" class="table table-striped table-bordered">
