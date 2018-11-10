56b1ed9b3edaceefbda97f154e653b98b5994ac4
Rob Holloway <robholloway@robbiechicago@gmail.com>
2018-11-10 15:32:48 +0000

    lots of styling changes to homepage

--- resources/views/partials/partial_home_sb_week_table.blade.php
+++ resources/views/partials/partial_home_sb_week_table.blade.php
@@ -1,28 +1,30 @@
 <div id="home-rh-table-week" class="col-md-12 home-rh-table">
-    <h3>Week {{ $latest_completed_week_num }} table</h3>
-    <em><a href="/weekly-scores/{{ $latest_completed_week_num }}">Click here for full table</a></em>
-    <table id="home-league-table" class="table table-sm">
-        <thead>
-            <tr>
-                <th>&nbsp;</th>
-                <th>Player</th>
-                <th># Res</th>
-                <th># Scr</th>
-                <th>Total Points</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($w = 0; $w < 5; $w++)
-                @if (isset($latest_week_scores[$w]))
+    <div class="sb-table-inner-div">
+        <h3>Week {{ $latest_completed_week_num }} table</h3>
+        <em><a href="/weekly-scores/{{ $latest_completed_week_num }}">Click here for full table</a></em>
+        <table id="home-league-table" class="table table-sm">
+            <thead>
                 <tr>
-                    <td>{{ $w + 1 }}</td>
-                    <td>{{ $latest_week_scores[$w]['name'] }}</td>
-                    <td>{{ $latest_week_scores[$w]['num_correct_res'] }}</td>
-                    <td>{{ $latest_week_scores[$w]['num_correct_scr'] }}</td>
-                    <td>{{ $latest_week_scores[$w]['tot_pts_won'] }}</td>
+                    <th>&nbsp;</th>
+                    <th>Player</th>
+                    <th># Res</th>
+                    <th># Scr</th>
+                    <th>Total Points</th>
                 </tr>
-                @endif
-            @endfor
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @for ($w = 0; $w < 5; $w++)
+                    @if (isset($latest_week_scores[$w]))
+                    <tr>
+                        <td>{{ $w + 1 }}</td>
+                        <td>{{ $latest_week_scores[$w]['name'] }}</td>
+                        <td><center>{{ $latest_week_scores[$w]['num_correct_res'] }}</center></td>
+                        <td><center>{{ $latest_week_scores[$w]['num_correct_scr'] }}</center></td>
+                        <td><center>{{ $latest_week_scores[$w]['tot_pts_won'] }}</center></td>
+                    </tr>
+                    @endif
+                @endfor
+            </tbody>
+        </table>
+    </div>
 </div>
